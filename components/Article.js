458cc88399c65668c4d7c849918e55f411af50e6
Trai Lynne <trailynne@gmail.com>
2021-03-09 19:48:45 -0700

    Complete Part 1 & 2 Add comments for future use

--- components/Article.js
+++ components/Article.js
@@ -3,18 +3,22 @@ import data from '../data/Articles';
 let container = document.querySelector('.articles');
 
 function articleMaker(article){
+  // Create Article 
   let newArticle = document.createElement('div');
   newArticle.classList.add('article');
 
+  // Add title 
   let title = document.createElement('h2');
   title.innerText = article.title;
   newArticle.appendChild(title);
 
+  // Add date
   let date = document.createElement('p');
   date.classList.add('date');
   date.innerText = article.date;
   newArticle.appendChild(date);
 
+  // Add Paragraphs
   let p1 = document.createElement('p');
   p1.innerText = article.firstParagraph;
   newArticle.appendChild(p1);
@@ -27,6 +31,8 @@ function articleMaker(article){
   p3.innerText = article.thirdParagraph;
   newArticle.appendChild(p3);
 
+  // Add Expand Button 
+  let dynamicClass = 'article-open'
   let span = document.createElement('span');
   span.classList.add('expandButton');
   span.addEventListener('click', e => {
@@ -34,16 +40,19 @@ function articleMaker(article){
 
     let parent = e.target.parentElement
 
-    parent.classList.contains('article-open') ?
-      e.path[1].classList.remove('article-open')
-      : e.path[1].classList.add('article-open');
+    parent.classList.contains(dynamicClass) ?
+      parent.classList.remove(dynamicClass)
+      : e.path[1].classList.add(dynamicClass);
   });
   newArticle.appendChild(span);
 
+  // Return 
   return newArticle;
 }
 
 
+
+// Add articles to DOM from data file 
 data.forEach(a => {
   container.appendChild(articleMaker(a));
 });
